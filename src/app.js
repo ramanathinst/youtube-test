d2d6602a5f0fb39a5af62ce3bc668853b209203b
pract-react <ramanathinst@gmail.com>
2024-08-04 18:18:48 +0530

    Add user controller endpoint

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-
-
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
     credentials: true,
@@ -15,6 +13,14 @@ app.use(express.urlencoded({extended: true , limit : "16kb" }))
 app.use(express.static("public"))
 app.use(express.json({limit : "16kb"}))
 app.use(cookieParser())
+
+
+// Router Import
+import userRouter from "./routes/user.routes.js"
+
+
+// Router declaration 
+app.use("/api/v1/users" , userRouter)
 
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import { app } from "./app.js"
 import connectDB from "./db/index.js"
 import dovenv from "dotenv"
+
 
 dovenv.config({
     path : "./.env"
@@ -7,3 +9,10 @@ dovenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(" server is running || Listing on port :" , process.env.PORT)
+    })
+}).catch((error) => {
+    console.log("mongo connection faild!")
+})
